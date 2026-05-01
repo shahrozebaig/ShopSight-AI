a4a89b78fa784a90c4657c71f72b5f44c8634f74
Shahroze Baig <shahrozeb98@gmail.com>
2026-05-01 08:30:39 +0530

    Update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -18,11 +18,11 @@ export const getStyleRecommendations = async (preferences, products) => {
   const res = await axios.post(`${API}/ai/style-recommendations`, { preferences, products });
   return res.data;
 };
-export const sendChat = async (query, page = 1) => {
+export const sendChat = async (query, page = 1, gender = "all") => {
   const res = await axios.post(`${API}/chat/`, {
     message: query,
-    products: [],
-    page: page
+    page: page,
+    gender: gender
   });
   return res.data;
 };
